508782b9df2d374f94dbab86813bfcef319ee62c
Legacy <118304022+Chocorrolito13@users.noreply.github.com>
2026-02-09 14:35:16 -0600

    Add files via upload

---
+++ Github.java
@@ -0,0 +1,10 @@
+public class Integrantes {
+
+    public static void main(String[] args) {
+
+        System.out.println("1. Enrique Rodriguez Guillen ");
+        System.out.println("2. Dominguez Hernandez Jose Francisco");
+        System.out.println("3. Manuel Alejandro Garcia Gome");
+
+    }
+}
